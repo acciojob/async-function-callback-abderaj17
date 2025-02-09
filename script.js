7d36b131cbe1e8730a8f56a72d173936d70608da
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-09 18:30:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
+const btn = document.querySelector("#btn");
+const outputF = document.querySelector("#output");
+
+const asyncFun = async ()=>{
+	const responce = await fetch(url);
+	const data = await responce.json();
+	
+}
 
 //your JS code here. If required.
